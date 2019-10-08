856f702c43c21c4d9ebdd7529ed01f312682deb2
Lizzy Reese <reeseelizabeth25@gmail.com>
2019-10-08 19:22:07 -0400

    main reducer built out

---
+++ react-game/src/reducers/main-reducer.js
@@ -0,0 +1,37 @@
+let initialState = { 
+    loggedIn: false,
+    questionId: 1,
+    score: 0,
+}
+
+let mainReducer = (state = initialState, action) => { 
+    if (action.type === 'LOGIN'){
+        return {
+        loggedIn: true, 
+        ...state
+        }
+    } else if (action.type === 'LOGOUT') {
+        return { 
+        loggedIn: false, 
+        ...state
+        }
+    } else if (action.type === 'NEXT_QUESTION') {
+        return {
+        ...state,
+        questionId: question + 1, 
+        }
+    } else if (action.type === 'RESTART') {
+        return { 
+            ...state,
+            questionId: 1
+        }
+    } else if (action.type === 'FINISH_GAME') {
+        return {
+            ...state, 
+            score: 0
+            // currently unfinished. will put in state when connected to api
+        }
+    }
+}
+
+export default mainReducer;
