ffa2bffda9f8b8bd3edacc0474c40de17ef48567
Anthony Gray <agray0232@gmail.com>
2019-10-10 19:11:56 -0400

    Made buttons a single button that is configurable

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
-import Buttons from './components/Buttons.jsx'
-import Resources from './components/Resources.jsx'
+import React from "react";
+import "./App.css";
+import Button from "./components/Button.jsx/index.js";
+import Resources from "./components/Resources.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-    <div className="Button-area">
-      <Buttons />
-    </div>
-    <div className="Resources-area">
-      <Resources />
-    </div>
+      <header className="App-header"></header>
+      <div className="Button-area">
+        <Button class="Restart-Button" text="Restart" />
+        <Button class="Next-Button" text="Next Question" />
+      </div>
+      <div className="Resources-area">
+        <Resources />
+      </div>
     </div>
   );
 }
