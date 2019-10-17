7fe62ee78e829658a593111995c792a207b49842
Lizzy Reese <reeseelizabeth25@gmail.com>
2019-10-17 19:47:22 -0400

    welcome screen functioning

--- react-game/src/components/Welcome.jsx
+++ react-game/src/components/Welcome.jsx
@@ -5,9 +5,10 @@ const Welcome = (props) => {
     return (
        
        <div className="Welcome">
-            <h1>Ready to ace your React Developer Interview?</h1>
+            <h1>Ready to ace your React Interview?</h1>
             <h2>Test you knowledge with our quiz to see how prepared you really are.</h2>
-          <button onclick={props.startGame} class="Start-button">Start the Game!</button>
+            <img src={process.env.PUBLIC_URL + '/cartoon.png'}></img>
+          <button onClick={props.startGame} className="Start-button">Start the Game!</button>
         </div>
         
     )}
